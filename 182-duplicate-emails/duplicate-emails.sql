be3a7878fd4dad63f7a39c254e04adc47d4a63e2
Sudhanshu Chaubey <32365537+Sud-1699@users.noreply.github.com>
2026-07-24 19:03:15 +0530

    Time: 439 ms (18.76%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,9 +1,14 @@
 # Write your MySQL query statement below
 
-SELECT DISTINCT(email)
+/* SELECT DISTINCT(email)
 FROM Person p1
 WHERE email IN (
     SELECT email
     FROM Person p2
     WHERE p1.id != p2.id
-)
+); */
+
+SELECT DISTINCT(p1.email)
+FROM Person p1
+JOIN Person p2 ON p1.email = p2.email
+WHERE p1.id != p2.id;
